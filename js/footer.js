d01df1ea83d0f88497b88fff9318b20cb688ea8d
tieukiemhp@gmail.com <tieukiemhp@gmail.com@99dc6f48-a063-fed3-6146-83903db3de8e>
2012-07-02 19:33:16 +0000

    footer.js

---
+++ js/footer.js
@@ -0,0 +1,12 @@
+//plusone
+(function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+//tynt.com
+if(document.location.protocol=='http:'){
+ var Tynt=Tynt||[];Tynt.push('aBLHhokRir4knmadbi-bnq');Tynt.i={"ap":"Xem thêm tại đây:","st":true};
+ (function(){var s=document.createElement('script');s.async="async";s.type="text/javascript";s.src='http://tcr.tynt.com/ti.js';var h=document.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})();
+}
+document.write('<img src="http://whos.amung.us/swidget/s6rbyr7qezmq.png" border="0" width="1" height="1" alt="User Online" />');
