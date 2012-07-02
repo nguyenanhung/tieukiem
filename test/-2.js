43180fb35466f3f9cd7aec0b38077c00eb3230f8
tieukiemhp@gmail.com <tieukiemhp@gmail.com@99dc6f48-a063-fed3-6146-83903db3de8e>
2012-07-02 19:52:03 +0000

    settisngs

--- test/-2.js
+++ test/-2.js
@@ -1,1 +1,1 @@
-<script language="javascript" type="text/javascript" src="http://tieukiem.googlecode.com/svn/js/settings.js"></script>
+<script language="javascript" type="text/javascript" src="http://tieukiem.googlecode.com/svn/js/settisngs.js"></script>
