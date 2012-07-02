6e7fb9511f0a19c496e29ed2659a919ce2894481
tieukiemhp@gmail.com <tieukiemhp@gmail.com@99dc6f48-a063-fed3-6146-83903db3de8e>
2012-07-02 20:09:36 +0000

    tieukiem

---
+++ js/addthis.js
@@ -0,0 +1,2 @@
+document.write('<div class="addthis_toolbox addthis_default_style "><a class="addthis_button_facebook_like" fb:like:layout="button_count"></a><a class="addthis_button_google_plusone" g:plusone:size="medium"></a></div>');
+document.write('<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=phimhp"></script>');
