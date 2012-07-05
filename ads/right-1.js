740c6476e6155121bd1a02b7dae1ea6fc7523eb2
nguyenanhung.5891@gmail.com <nguyenanhung.5891@gmail.com@99dc6f48-a063-fed3-6146-83903db3de8e>
2012-07-05 17:16:29 +0000

    change type

--- ads/right-1.js
+++ ads/right-1.js
@@ -1,13 +1,13 @@
 /*
 quảng cáo Ad360.vn - hiển thị từ 9h đến 12h đêm
-*/
 var _ad360_id=846;
 var _ad360_w=300;
 var _ad360_h=600;
 var _ad360_pos=0;
 document.write('<script language="javascript" type="text/javascript" src="http://provider.ad360.vn/showads.min.js"></script>');
+*/
 
 /*
 quảng cáo adVatgia - hiển thị ban ngày
-document.write('<div id="adVatgia_block_1"></div>');
 */
+document.write('<div id="adVatgia_block_1"></div>');
