9814c7f8528e075d326f404fb2f62c977152af8f
tieukiemhp@gmail.com <tieukiemhp@gmail.com@99dc6f48-a063-fed3-6146-83903db3de8e>
2012-07-05 15:07:08 +0000

    ads/f-right.js

---
+++ ads/f-right.js
@@ -0,0 +1,5 @@
+var _ad360_id=835;
+var _ad360_w=120;
+var _ad360_h=600;
+var _ad360_pos=9;
+document.write('<script language="javascript" type="text/javascript" src="http://provider.ad360.vn/showads.min.js"></script>');
