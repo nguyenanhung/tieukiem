989f5f12159fc5360b35a656b0ded0dbff31920b
tieukiemhp@gmail.com <tieukiemhp@gmail.com@99dc6f48-a063-fed3-6146-83903db3de8e>
2012-07-05 14:56:57 +0000

    vatgia

--- ads/right-1.js
+++ ads/right-1.js
@@ -1,7 +1,7 @@
-var _ad360_id=846;
+/*var _ad360_id=846;
 var _ad360_w=300;
 var _ad360_h=600;
 var _ad360_pos=0;
 document.write('<script language="javascript" type="text/javascript" src="http://provider.ad360.vn/showads.min.js"></script>');
-
-//document.write('<div id="adVatgia_block_1"></div>');
+*/
+document.write('<div id="adVatgia_block_1"></div>');
