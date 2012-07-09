e21a8284ec4d46c4270644fdbd5c166c74456236
nguyenanhung.5891@gmail.com <nguyenanhung.5891@gmail.com@99dc6f48-a063-fed3-6146-83903db3de8e>
2012-07-09 11:37:30 +0000

    qc/ad360/float-left.js

---
+++ qc/ad360/float-left.js
@@ -0,0 +1,6 @@
+/* Ad360 - float-left.js */
+var _ad360_id=833;
+var _ad360_w=120;
+var _ad360_h=600;
+var _ad360_pos=7;
+document.write('<script language="javascript" type="text/javascript" src="http://provider.ad360.vn/showads.min.js"></script>');
