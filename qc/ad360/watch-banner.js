41eadd7537174fb6dc074060dfd9ffb3ee7011cf
nguyenanhung.5891@gmail.com <nguyenanhung.5891@gmail.com@99dc6f48-a063-fed3-6146-83903db3de8e>
2012-07-09 12:22:13 +0000

    qc/ad360/watch-banner.js

--- qc/ad360/watch-banner.js
+++ qc/ad360/watch-banner.js
@@ -1,6 +1,6 @@
 /* Ad360 - watch-banner.js */
-var _ad360_id=881;
-var _ad360_w=685;
+var _ad360_id=885;
+var _ad360_w=640;
 var _ad360_h=90;
 var _ad360_pos=0;
 document.write('<script language="javascript" type="text/javascript" src="http://provider.ad360.vn/showads.min.js"></script>');
