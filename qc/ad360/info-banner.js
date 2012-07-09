3079f8e843f9624ea3ba4eea597505367483b036
nguyenanhung.5891@gmail.com <nguyenanhung.5891@gmail.com@99dc6f48-a063-fed3-6146-83903db3de8e>
2012-07-09 11:57:16 +0000

    qc/ad360/info-banner.js

---
+++ qc/ad360/info-banner.js
@@ -0,0 +1,6 @@
+/* Ad360 - info-banner.js */
+var _ad360_id=880;
+var _ad360_w=675;
+var _ad360_h=90;
+var _ad360_pos=0;
+document.write('<script language="javascript" type="text/javascript" src="http://provider.ad360.vn/showads.min.js"></script>');
