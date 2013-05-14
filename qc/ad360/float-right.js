419d06aa48cdde318f4daca8b041f3813b75b020
nguyenanhung.5891@gmail.com <nguyenanhung.5891@gmail.com@99dc6f48-a063-fed3-6146-83903db3de8e>
2013-05-14 19:51:12 +0000

    var _ad360_pos=1;

--- qc/ad360/float-right.js
+++ qc/ad360/float-right.js
@@ -2,5 +2,5 @@
 var _ad360_id=835;
 var _ad360_w=120;
 var _ad360_h=600;
-var _ad360_pos=9;
+var _ad360_pos=1;
 document.write('<script language="javascript" type="text/javascript" src="http://provider.ad360.vn/showads.min.js"></script>');
