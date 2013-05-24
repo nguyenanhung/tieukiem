fa4c815b60c618ddeb07c39b3edb9a3a1274c862
nguyenanhung.5891@gmail.com <nguyenanhung.5891@gmail.com@99dc6f48-a063-fed3-6146-83903db3de8e>
2013-05-24 00:49:17 +0000

    qc/ad360/right07.js

---
+++ qc/ad360/right07.js
@@ -0,0 +1,6 @@
+/* Ad360 - zone/right07.js */
+var _ad360_id=878;
+var _ad360_w=300;
+var _ad360_h=250;
+var _ad360_pos=0;
+document.write('<script language="javascript" type="text/javascript" src="http://provider.ad360.vn/showads.min.js"></script>');
