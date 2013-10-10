919e1957fabdf473162660e969b5ad3ff210d662
nguyenanhung.5891@gmail.com <nguyenanhung.5891@gmail.com@99dc6f48-a063-fed3-6146-83903db3de8e>
2013-10-10 19:18:59 +0000

    qc/novanet/zone1.js

---
+++ qc/novanet/zone1.js
@@ -0,0 +1,6 @@
+/* Novanet - zone1.js */
+var novanet_pubid=394;
+var novanet_zoneid=1600;
+var novanet_width=300;
+var novanet_height=250;
+document.write('<script language="javascript" type="text/javascript" src="http://static.novanet.vn/embed.js"></script>');
