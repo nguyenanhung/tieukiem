8682f0f3efbce5474b4d97a678b4425197585eca
nguyenanhung.5891@gmail.com <nguyenanhung.5891@gmail.com@99dc6f48-a063-fed3-6146-83903db3de8e>
2014-03-04 20:11:03 +0000

    214967728696915

--- blogspot/footer.js
+++ blogspot/footer.js
@@ -9,6 +9,6 @@
 var js, fjs = d.getElementsByTagName(s)[0];
 if (d.getElementById(id)) return;
 js = d.createElement(s); js.id = id;
-js.src = "//connect.facebook.net/vi_VN/all.js#xfbml=1&appId=149730545131397";
+js.src = "//connect.facebook.net/vi_VN/all.js#xfbml=1&appId=214967728696915";
 fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
